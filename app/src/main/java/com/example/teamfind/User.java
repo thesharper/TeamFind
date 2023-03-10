9c1fdc13a4761c2a483738181fad2ce527ab160c
thesharper <sreniy06@gmail.com>
2023-03-10 11:01:15 +0700

    Add project page

--- app/src/main/java/com/example/teamfind/User.java
+++ app/src/main/java/com/example/teamfind/User.java
@@ -6,6 +6,7 @@ public class User {
     public String name;
     public Category[] categories;
     private ZonedDateTime zt;
+    public Project[] projects;
 
     public User(String name) {
         this.name = name;
